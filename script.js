e25719ab0889dd891613ec6c26595775f2194ae7
Renekris <renekrispohlak@gmail.com>
2022-08-18 11:59:22 +0300

    Change layout, add outline toggle and hold to draw

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const gridContainer = document.getElementById('grid-container')
+let primaryMouseButtonDown = false;
 let currentColor = '255, 0, 0,';
 let currentOpacity = '1';
+
+document.addEventListener("mousedown", setPrimaryButtonState);
+document.addEventListener("mousemove", setPrimaryButtonState);
+document.addEventListener("mouseup", setPrimaryButtonState);
+
+// Can only draw when m1 held down
+function setPrimaryButtonState(e) {
+  let flags = e.buttons !== undefined ? e.buttons : e.which;
+  primaryMouseButtonDown = (flags & 1) === 1;
+}
 
 function createGrid(gridX, gridY){
     for (let y = 0; y < gridY; y++) {
         const divRow = document.createElement('div');
         for (let x = 0; x < gridX; x++) {
             const cell = document.createElement('div');
-            cell.className = 'grid-cell';
+            cell.className = 'grid-cell cell-outline';
             divRow.append(cell);
         }
         divRow.className = 'grid-row';
@@ -18,21 +29,21 @@ function createGrid(gridX, gridY){
 function addGridListener() {
     const gridCells = document.getElementsByClassName('grid-cell');
     Array.from(gridCells).forEach(cell => {
-        cell.addEventListener('pointerover', e => colorGridCell(e));
+        cell.addEventListener('pointerrawupdate', e => colorGridCell(e));
     });
 }
 
 // Have to use rgba, as opacity property slows things down.
 function colorGridCell(e) {
-    e.target.style.backgroundColor = `rgba(${currentColor} ${currentOpacity})`
+    if (primaryMouseButtonDown) {
+        e.target.style.backgroundColor = `rgba(${currentColor} ${currentOpacity})`
+    }
 }
 
 function resetGrid() {
     gridContainer.innerHTML = '';
     drawGrid();
 }
-
-
 
 function drawGrid() {
     createGrid(100, 100);
@@ -42,11 +53,18 @@ function drawGrid() {
 const resetButton = document.querySelector('#reset button');
 resetButton.addEventListener('click', () => resetGrid());
 
-const opacityRange = document.getElementById('opacity');
-opacityRange.addEventListener('click', e => {
+const opacityInput = document.getElementById('opacity');
+opacityInput.addEventListener('click', e => {
     currentOpacity = e.target.value / 10;
     console.log(currentOpacity);
 } )
 
+const outlineInput = document.getElementById('outline');
+outlineInput.addEventListener('click', () => {
+    const cells = document.getElementsByClassName('grid-cell');
+    Array.from(cells).forEach(cell => {
+        cell.classList.toggle('cell-outline');
+    })
+})
 
 drawGrid()
